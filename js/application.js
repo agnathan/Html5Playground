155c4a196254c4aca3eb3e69ace3d53c53705950
Daniel Holmlund <holmlund.dev@gmail.com>
2011-11-02 12:00:05 -0700

    tweaks

--- js/application.js
+++ js/application.js
@@ -62,6 +62,31 @@ $(document).ready(function () {
     // NOTE: this is only necessary because we are changing CSS *AFTER LOADING* using themeSwitcher
     //setTimeout( pageLayout.resizeAll, 2000 ); /* allow time for browser to re-render with new theme */
 
+    /* Jquery for the menus */
+// $(document).ready(function(){
+
+// 	$("ul.subnav").parent().append("<span></span>"); //Only shows drop down trigger when js is enabled (Adds empty span tag after ul.subnav*)
+
+// 	$("ul.topnav li span").click(function() { //When trigger is clicked...
+
+// 		//Following events are applied to the subnav itself (moving subnav up and down)
+// 		$(this).parent().find("ul.subnav").slideDown('fast').show(); //Drop down the subnav on click
+
+// 		$(this).parent().hover(function() {
+// 		}, function(){
+// 			$(this).parent().find("ul.subnav").slideUp('slow'); //When the mouse hovers out of the subnav, move it back up
+// 		});
+
+// 		//Following events are applied to the trigger (Hover events for the trigger)
+// 		}).hover(function() {
+// 			$(this).addClass("subhover"); //On hover over, add class "subhover"
+// 		}, function(){	//On Hover Out
+// 			$(this).removeClass("subhover"); //On hover out, remove class "subhover"
+// 	});
+
+// });
+
+
     /* Dyanmically build the sidebar */
     var sidebar = $("#example_sidebar");
     $.each(example_data, function(index, ex) { 
